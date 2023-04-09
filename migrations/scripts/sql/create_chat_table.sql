eda940fca311ba45615d046a6d9c90540b4532a4
Александр Бахмутов <alexdmitralex@gmail.com>
2023-04-09 13:42:45 +0300

    Добавил скрипт для создания таблицы чатов

---
+++ migrations/scripts/sql/create_chat_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS chat
+(
+    chat_id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    tg_chat_id BIGINT NOT NULL UNIQUE ,
+
+    CONSTRAINT PK_chat_chat_id PRIMARY KEY (chat_id)
+);
