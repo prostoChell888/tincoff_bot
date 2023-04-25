41cb0a7966082b15d4778b4d00281ecd59a7c8be
Александр Бахмутов <alexdmitralex@gmail.com>
2023-04-25 21:12:44 +0300

    завершил 6 практику

---
+++ generate/src/test/java/com/example/jooccodegen/JoocCodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jooccodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoocCodeGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
