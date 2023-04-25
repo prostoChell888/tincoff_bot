a630fd1d353ffb325644928a6dc6a3c199721c37
Александр Бахмутов <alexdmitralex@gmail.com>
2023-04-25 23:46:05 +0300

    add Links repo

--- scrapper/src/main/java/ru/tinkoff/scrapper/repository/JPA/LinkJPARepository.java
+++ scrapper/src/main/java/ru/tinkoff/scrapper/repository/JPA/LinkJPARepository.java
@@ -3,9 +3,19 @@ package ru.tinkoff.scrapper.repository.JPA;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.tinkoff.scrapper.enyity.JPA.JPAChatEntity;
+import ru.tinkoff.scrapper.enyity.JPA.JPALinkEntity;
 import ru.tinkoff.scrapper.enyity.LinkEntity;
 
-@Repository
-public interface LinkJPARepository extends JpaRepository<LinkEntity, Long> {
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface LinkJPARepository extends JpaRepository<JPALinkEntity, Long> {
+
+
+    List<JPALinkEntity> findAllByChats(JPAChatEntity link);
+
+    Optional<JPALinkEntity> findByLink(URI link );
 }
---
+++ scrapper/src/main/java/ru/tinkoff/scrapper/service/JPA/LinksJpaService.java
@@ -0,0 +1,74 @@
+package ru.tinkoff.scrapper.service.JPA;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.tincoff.exeptios.NotFoundException;
+import ru.tinkoff.scrapper.dto.request.AddLinkRequest;
+import ru.tinkoff.scrapper.dto.responce.link.LinkResponse;
+import ru.tinkoff.scrapper.dto.responce.link.ListLinksResponse;
+import ru.tinkoff.scrapper.enyity.JPA.JPALinkEntity;
+import ru.tinkoff.scrapper.repository.JPA.LinkJPARepository;
+import ru.tinkoff.scrapper.repository.JPA.TgChatJPARepository;
+import ru.tinkoff.scrapper.service.LinkService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class LinksJpaService implements LinkService {
+
+    private final LinkJPARepository linkRepository;
+    private final TgChatJPARepository chatRepository;
+
+
+    @Override
+    public LinkResponse add(Long tgChatId, AddLinkRequest request) {
+        var chatOptional = chatRepository.findByTgChatId(tgChatId);
+        if (chatOptional.isEmpty() ){
+            throw new NotFoundException("Чат с id" + tgChatId + " не найден");
+        }
+        var chat = chatOptional.get();
+
+        var linkOptional = linkRepository.findByLink(request.link()) ;
+        var link = linkOptional.get();
+        if (linkOptional.isEmpty()) {
+            link = new JPALinkEntity();
+            link.setLink(request.link());
+            link = linkRepository.save(link);
+        }
+        chat.getLinks().add(link);
+        chatRepository.save(chat);
+
+        return new LinkResponse(link.getLinkId(), request.link());
+
+    }
+
+    @Override
+    public ListLinksResponse listAll(Long tgChatId) {
+        var chatOption = chatRepository.findByTgChatId(tgChatId);
+        if (chatOption.isEmpty() ){
+            throw new NotFoundException("Чат с id" + tgChatId + " не найден");
+        }
+        var links = linkRepository.findAllByChats(chatOption.get());
+
+        List<LinkResponse> chatIds = new ArrayList<>();
+        for (var link:links) {
+            chatIds.add(new LinkResponse(link.getLinkId(), link.getLink()));
+        }
+        return new ListLinksResponse(chatIds, chatIds.size());
+    }
+
+    @Override
+    public void remove(Long chatId) {
+        var chatOption = chatRepository.findById(chatId);
+        if (chatOption.isEmpty() ){
+            throw new NotFoundException("Чат с id" + chatId + " не найден");
+        }
+
+        chatRepository.deleteById(chatId);
+
+
+    }
+}
