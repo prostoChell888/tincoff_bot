178fdb8c92090115565f1b7bed13bdec1f4608a4
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-06 10:30:43 +0300

    Задание 5: добавил classMapper

--- bot/src/main/java/ru/tinkoff/bot/configuration/RabbitMQConfiguration.java
+++ bot/src/main/java/ru/tinkoff/bot/configuration/RabbitMQConfiguration.java
@@ -5,10 +5,16 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.ClassMapper;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.tinkoff.bot.dto.request.LinkUpdateRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RabbitMQConfiguration {
@@ -33,5 +39,23 @@ public class RabbitMQConfiguration {
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ClassMapper classMapper(){
+        Map<String, Class<?>> mappings = new HashMap<>();
+        mappings.put("ru.tinkoff.bot.dto.request.LinkUpdateRequest", LinkUpdateRequest.class);
+
+        DefaultClassMapper classMapper = new DefaultClassMapper();
+        classMapper.setTrustedPackages("ru.tinkoff.bot.dto.request.*");
+        classMapper.setIdClassMapping(mappings);
+        return classMapper;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(ClassMapper classMapper){
+        Jackson2JsonMessageConverter jsonConverter=new Jackson2JsonMessageConverter();
+        jsonConverter.setClassMapper(classMapper);
+        return jsonConverter;
     }
 }
