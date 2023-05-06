709f8e4c4e4dfdc77f7433de878b5e7433f8f461
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-06 10:06:06 +0300

    Задание 5: добавил класс ScrapperQueueListener

---
+++ bot/src/main/java/ru/tinkoff/bot/service/ScrapperQueueListener.java
@@ -0,0 +1,19 @@
+package ru.tinkoff.bot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import ru.tinkoff.bot.dto.request.LinkUpdateRequest;
+
+
+@RabbitListener(queues = "${scrapper.scrapper-queue.name}")
+@RequiredArgsConstructor
+public class ScrapperQueueListener {
+
+    private final BotService botService;
+
+    @RabbitHandler
+    public void receiver(LinkUpdateRequest update) {
+        botService.update(update);
+    }
+}
