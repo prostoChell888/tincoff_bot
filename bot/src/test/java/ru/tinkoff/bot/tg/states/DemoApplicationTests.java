d2c40b9523f7552afd5acd539037b846f27828f6
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-09 16:06:46 +0300

    1: добавил тест

---
+++ bot/src/test/java/ru/tinkoff/bot/tg/states/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.tinkoff.bot.tg.states;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertEquals(1, 1);
+    }
+
+}
